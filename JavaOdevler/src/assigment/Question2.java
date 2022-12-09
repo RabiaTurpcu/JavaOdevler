e932bed3554e35f21c6a356724e82196e5875231
Rabiaturpcu <rabiaturpcu096@gmail.com>
2022-12-09 14:43:12 +0100

    First Java Homework

---
+++ JavaOdevler/src/assigment/Question2.java
@@ -0,0 +1,22 @@
+package assigment;
+
+public class Question2 {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Byte max deger " + Byte.MAX_VALUE + "\nByte Min " + Byte.MIN_VALUE);
+		
+		System.out.println("Short max deger " + Short.MAX_VALUE + "\nShort min deger " + Short.MIN_VALUE); 
+		
+		System.out.println("Integer max deger " + Integer.MAX_VALUE + "\nInteger min deger " + Integer.MIN_VALUE);
+		
+		System.out.println("Long max deger " + Long.MAX_VALUE + "\nLong min deger " + Long.MIN_VALUE);
+		
+		System.out.println("Float max deger " + Float.MAX_VALUE + "\nFloat min deger " + Float.MIN_VALUE);
+		
+		System.out.println("Double max deger "+ Double.MAX_VALUE + "\nDouble min deger " + Double.MIN_VALUE);
+		
+	}
+
+}
